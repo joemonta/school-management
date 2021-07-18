f37f9fb91472f7012194bb56bd9bb283401e5e0b
Joemon <joemon@joemon>
2021-07-18 11:12:45 +0530

    Third Commit Branch Commit

--- student/src/main/java/com/joe/student/StudentMain.java
+++ student/src/main/java/com/joe/student/StudentMain.java
@@ -4,6 +4,12 @@ import com.joe.address.Address;
 
 public class StudentMain {
 
+    private String student;
+
+    public StudentMain(String student) {
+        this.student = student;
+    }
+
     public static void main(String[] args) {
         Address address = new Address();
         System.out.println(address.getAddress());
